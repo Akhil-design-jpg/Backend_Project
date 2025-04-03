08089e19cdbf6ce1afc78f69f9bee0a884e365a4
Akhil_Verma <74674142+Akhil-design-jpg@users.noreply.github.com>
2025-04-03 12:28:35 +0530

    Update asyncHandler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -9,6 +9,36 @@ const asyncHandler = (requestHandler) =>{
 export {asyncHandler}
 
 
+/*
+    asynchandler
+    a higher order function that takes another function as an argument and possible returns a new function
+
+    Why needed? 
+    In express.js, if an asynchronous function throws an error, you need to explicitly pass that error to the next function 
+    for the express error-handling middleware to catch it. Without this, your server might crash or not handle the error properly
+
+    different variations
+
+    version1 : The normal function 
+    const asyncHandler = () =>{}
+
+    version2: The higer-order function 
+    const asyncHandler = (func)=>{
+    return () =>{}
+    Takes a function(func) as an argument and returns a new function 
+
+    version3: The higher-order async function
+    const asyncHandler = (func) =>{
+        return async() =>{}
+    
+    }
+
+    Same as above, but the returned function is asynchronous (async).
+    Used when the inner function (func) involves asynchronous operations
+
+/
+
+
 
 
 // const asyncHandler = () =>{}
