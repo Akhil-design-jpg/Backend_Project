dc363351a361a728a3fad915fb278a32ebf85fb4
Akhil_Verma <74674142+Akhil-design-jpg@users.noreply.github.com>
2025-04-03 11:57:08 +0530

    Update ApiResponse.js

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -5,7 +5,7 @@ class ApiResponse{
         this.statusCode = statusCode
         this.data = data
         this.message = message
-        this.success = success < 400
+        this.success = statusCode < 400
 
         /*
             Informational responses(100-199) statusCode
